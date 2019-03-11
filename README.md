0f3359a4074d74eb430ef460de7a60ded1f7fe66
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-11 19:58:31 +0700

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script accepts a path to a text file and outputs 10 most frequent words to the console.
 
+# Quickstart
+
+The script requires for its operation installed Python interpreter version 3.5.
+The path to the text file is specified when running after the script name
+
+Example of script lauch on Linux, Python 3.5:
+
+```bash
+$ python lang_frequency.py text.txt 
+Десять самых популярных слов в тексте:
+the, he, a, was, to, and, of, it, dursley, his
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import sys
+from collections import Counter
+import re
 
 
 def load_data(filepath):
-    pass
+    words_list = []
+    with open(filepath, 'r') as file_handler:
+        for line in file_handler:
+            words_list += [elem.lower() for elem in re.findall(r'\w+', line)]
+    return words_list
 
 
 def get_most_frequent_words(text):
-    pass
+    counter = Counter(text)
+    return [elem[0] for elem in counter.most_common(10)]
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+    else:
+        print('Не указан путь к файлу')
+        exit()
+
+    print('Десять самых популярных слов в тексте:')
+    print(*get_most_frequent_words(load_data(filepath)), sep=', ')
