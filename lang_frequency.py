fac4f0d4996ca5497a93870b5a1959470ac88633
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-13 00:03:10 +0700

    fix bugs

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,29 +1,41 @@
+import os
+import re
 import sys
 from collections import Counter
-import re
 
 
 def load_data(filepath):
-    words_list = []
-    with open(filepath, 'r') as file_handler:
-        for line in file_handler:
-            words_list += [elem.lower() for elem in re.findall(r'\w+', line)]
-    return words_list
+    if not os.path.exists(filepath) or not os.path.isfile(filepath):
+        return None
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file_handler:
+            text = file_handler.read()
+        return text
+    except UnicodeDecodeError:
+        return None
 
 
-def get_most_frequent_words(text, number_of_words):
-    counter = Counter(text)
-    return [elem[0] for elem in counter.most_common(number_of_words)]
+def process_data(text):
+    return [word for word in re.findall(r'\w+', text.lower())]
+
+
+def get_most_frequent_words(words, number_of_words):
+    counter = Counter(words)
+    return [word for word, count in counter.most_common(number_of_words)]
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         filepath = sys.argv[1]
     else:
-        print('Не указан путь к файлу')
-        exit()
+        sys.exit('Не указан путь к файлу')
+
+    text = load_data(filepath)
+
+    if not text:
+        exit('Файл не найден или данные не корректны')
 
     number_of_words = 10
-    print('Десять самых популярных слов в тексте:')
-    print(*get_most_frequent_words(load_data(filepath), number_of_words),
+    print('%s самых популярных слов в тексте:' % number_of_words)
+    print(*get_most_frequent_words(process_data(text), number_of_words),
           sep=', ')
